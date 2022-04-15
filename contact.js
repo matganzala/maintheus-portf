7c0f30c99723caa10b636a6d8c2fffa86a70c138
MagicGanza <tngmatheus@gmail.com>
2022-04-15 20:21:29 -0300

    config contact.js

---
+++ contact.js
@@ -0,0 +1,15 @@
+function email(){
+    console.log("Email")
+}
+
+function mensage(){
+    console.log("Mensagem")
+}
+
+function sendBtn(){
+    console.log("Mensagem enviada")
+}
+
+function cancelBtn(){
+    console.log("Mensagem cancelada")
+}
